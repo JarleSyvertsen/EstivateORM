062546a0e29db46f083d48c32001d62903ef6a17
Jarle <jarlesyv@hiof.no>
2023-03-22 09:11:56 +0100

    More defined SQLWriteObjects

--- src/main/java/hiof/gruppe1/Estivate/Objects/SQLWriteObject.java
+++ src/main/java/hiof/gruppe1/Estivate/Objects/SQLWriteObject.java
@@ -4,4 +4,16 @@ import java.util.HashMap;
 
 public class SQLWriteObject {
     HashMap<String, SQLAttribute> attributes;
+
+    public SQLWriteObject() {
+    }
+
+    public void addAttribute(String name, SQLAttribute<Object> attribute) {
+        attributes.put(name, attribute);
+    }
+
+    public HashMap<String, SQLAttribute> getAttributeList() {
+        return attributes;
+    }
 }
+
