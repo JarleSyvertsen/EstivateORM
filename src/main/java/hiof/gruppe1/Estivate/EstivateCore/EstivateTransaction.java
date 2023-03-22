d653bc235b4f00a90d9caf75f92ffaa0760a02e1
Jarle <jarlesyv@hiof.no>
2023-03-22 11:28:02 +0100

    Spacing.

--- src/main/java/hiof/gruppe1/Estivate/EstivateCore/EstivateTransaction.java
+++ src/main/java/hiof/gruppe1/Estivate/EstivateCore/EstivateTransaction.java
@@ -12,15 +12,17 @@ public class EstivateTransaction {
     Connection connection;
 
     public EstivateTransaction startTransaction() {
-    // SQL Start transaction
-    return this;
+        // SQL Start transaction
+        return this;
     }
 
     public void persist(Object object) {
         return;
     }
 
-    public void commit() {}
+    public void commit() {
+    }
+
     public void endTransaction() {
         // End transaction
         // Could empty out variables as well, considering this object should not be reused.
